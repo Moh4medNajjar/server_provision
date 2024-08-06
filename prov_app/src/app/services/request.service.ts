2032552f9d11fee61282c32d2fc119c85a6f7095
Moh4medNajjar <134518701+Moh4medNajjar@users.noreply.github.com>
2024-08-07 00:06:53 +0100

    Update request.service.ts

--- prov_app/src/app/services/request.service.ts
+++ prov_app/src/app/services/request.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class RequestService {
 
-  private apiUrl = 'http://192.168.1.168:3000/api/requests'; // Replace with your API URL
+  private apiUrl = 'http://192.168.1.212:3000/api/requests'; // Replace with your API URL
 
   constructor(private http: HttpClient, private authService: AuthService) { }
 
